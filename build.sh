b6b662fe55a3c2d2563a6ff269aa01956aa0b34d
Ian Gibbs <90706300+idigs@users.noreply.github.com>
2024-01-19 11:20:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 findmnt
 lsblk
 env
+
+
+sudo apt install -y libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc curl build-essential gcc bc bison flex libssl-dev libncurses5-dev libelf-dev
+
