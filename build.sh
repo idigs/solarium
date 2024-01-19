2c30819f84aa0eaf55d3574f876941c46469c54a
Ian Gibbs <90706300+idigs@users.noreply.github.com>
2024-01-19 11:26:12 -0500

    remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-findmnt
-lsblk
-env
-
-
-sudo apt install -y libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc curl build-essential gcc bc bison flex libssl-dev libncurses5-dev libelf-dev
-
